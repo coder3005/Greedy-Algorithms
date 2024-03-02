e4657e0f6443724dc630e39ce358af8c4f80a814
ABHI GUPTA <138576028+coder3005@users.noreply.github.com>
2024-03-02 11:57:05 +0530

    Update maxlengthchainofpairs.java

--- maxlengthchainofpairs.java
+++ maxlengthchainofpairs.java
@@ -1,7 +1,7 @@
 import java.util.*;
 
 public class classroom {
-    public static void main(String[] args) {
+    public static void main(String[] args) { -//O(nlogn)
         int pairs[][]={{5,24},{39,60},{5,28},{27,40},{50,90}};
 
         Arrays.sort(pairs,Comparator.comparingDouble(o -> o[1]));
