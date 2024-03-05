422d75ad81bfc19e2e52485837f2501726f4a159
ABHI GUPTA <138576028+coder3005@users.noreply.github.com>
2024-03-05 22:19:56 +0530

    Create buy&sellstock.java

---
+++ buy&sellstock.java
@@ -0,0 +1,24 @@
+//best time to buy and sell stock
+
+public class classroom {
+    public static int stock(int prices[]) {
+        int maxProfit=0;
+        int minPrice=prices[0];
+        for(int i=1;i<prices.length;i++) {
+            if(prices[i]<minPrice) {
+                minPrice=prices[i];
+            }
+            else {
+                int profit=prices[i]-minPrice;
+                if(profit>maxProfit) {
+                    maxProfit=profit;
+                }
+            }
+        }
+        return maxProfit;
+    }
+    public static void main(String[] args) {
+        int prices[]={7,1,5,3,6,4};
+        System.out.println(stock(prices));
+    }
+}
